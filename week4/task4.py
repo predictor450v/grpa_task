a6df07d80c58397bd394d043cbe20d130d3bc963
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-01-23 21:47:11 +0530

    changed

--- week4/task4.py
+++ week4/task4.py
@@ -1,24 +1,3 @@
-'''
-You are required to complete the following functions to perform various operations on tuples and lists.
-
-swap_halves: Swap the first and second halves of a tuple with an even length.
-Input: A tuple of even length.
-Output: A new tuple where the first and second halves are swapped.
-swap_at_index: Break a tuple at a given index ( k ) (the element at the ( k )-th index is included in the first half before swapping) and swap the parts.
-Input: A tuple and an integer ( k ).
-Output: A new tuple where the parts before and after the ( k )-th index are swapped.
-rotate_k: Create a new list with elements of the given list moved ( k ) positions towards the right. The elements at the end should come back to the beginning in a circular order.
-Input: A list and an integer ( k ) (default value ( k = 1 )).
-Output: A new list with the elements rotated ( k ) positions to the right.
-first_and_last_index: Get the indices of the first and last occurrence of a given item in a list. Assume the item is present in the list at least once.
-Input: A list and an item.
-Output: A tuple with the first and last indices of the given item in the list.
-reverse_first_and_last_halves: Reverse the first and last halves of a list with even length in place.
-Input: A list with an even length.
-Output: None (the list should be modified in place).
-'''
-
-
 
 
 
